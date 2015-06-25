aaa2d4370d4a2ebaee96c4a60393ac7b1482dc46
Kim Eik <kim@heldig.org>
2015-06-26 00:56:49 +0200

    Updating site

--- public/update.sh
+++
@@ -1,4 +0,0 @@
-#hugo -t casper
-git add -A
-git commit -m "Updating site" && git push origin master
-git subtree push --prefix=public git@github.com:netbrain/me.git gh-pages
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #hugo -t casper
 git add -A
-git commit -m "Updating site" && git push origin master
-git subtree push --prefix=public git@github.com:netbrain/me.git gh-pages
+git commit -m "Updating site" && git push
+git subtree push --prefix=public git@github.com:netbrain/netbrain.github.com.git master
