33ccbf736e34837e70fa2ad88fe70eec84801d77
Kim Eik <kim@heldig.org>
2015-06-26 01:16:21 +0200

    Updating site

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-#hugo -t casper
+#!/bin/bash
+hugo -t casper
 git add -A
 git commit -m "Updating site" && git push
 git subtree push --prefix=public git@github.com:netbrain/netbrain.github.com.git master
